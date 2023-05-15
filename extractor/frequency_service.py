72f9980455618540b3928403a3685caff64d9d9d
Henny Sluyter-Gäthje <sluytergaeth@uni-potsdam.de>
2023-05-15 10:22:58 +0200

    changed output format, wordlist is now requested from url

--- extractor/frequency_service.py
+++ extractor/frequency_service.py
@@ -2,15 +2,14 @@
 
 """
 Preprocess list of strings (chapters or paragraphs of novels) **text** with spacy: Tokenization, Lemmatization
-Read in json with animal words and nature words 
-For each entry in **text* extract frequency of nature words
-Return dictionary with {paragraph1:{nature_word_1:frequency,...},...}
+Retrieve word info dict from given URL: {list_lemma: {ID:val},...}
+    this format is extendable with e.g. PoS info
+For each entry in **text* extract frequency of words in word info dict
+Return dictionary with {list_lemma:{ID: val, segment_frequency: {chapter_ID: frequency,...,} overall_frequency: frequency},...}
 """
 
 import sys
-import json
-from numpy import e
-from typing_extensions import Literal
+import requests
 from collections import Counter
 from enum import Enum
 
@@ -19,16 +18,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-DE_NATURE_WORDS = "../wordlists/nature_words.json"
-EN_NATURE_WORDS = "path/to/file"
-
 class Language(Enum):
     EN = "en"
     DE = "de"
 
-class NatureCategories(Enum):
-    Animal = "animal"
-    Plant = "plant"
+SEGMENT_FRQ = "segment_frequency"
+OVERALL_FRQ = "overall_frequency"
 
 @app.get("/")
 def root():
@@ -36,65 +31,69 @@ def root():
             'service': 'ecocor-metrics',
             'version': '0.0.0'
             }
-def read_json(fp):
-    with open(fp, 'r') as json_in:
-        loaded = json.load(json_in)
-    uniques = {key: set(val) for key, val in loaded.items()}
-    return uniques
 
+#TODO: check if the file belongs to a certain **secure** URL 
+def read_word_list(url):
+    response = requests.get(url).json()
+    return response
 
 def initialize_de():
      global nlp
      nlp = spacy.load('de_core_news_sm')
-     return read_json(DE_NATURE_WORDS)
 
 def initialize_en():
     global nlp
     nlp = spacy.load('en_core_web_sm')
-    return read_json(EN_NATURE_WORDS)
 
-def setup_analysis_components(language: str):
+def setup_analysis_components(language: str, resource_url:str) -> dict[str, dict[str,str]]:
     languages = {x.value for x in Language}
     assert language in languages, f"Language must be one of the following: {languages}" 
+    
+    id_word_info_dict = read_word_list(resource_url)
 
     if language == Language.DE.value:
-        nature_words = initialize_de()
+        initialize_de()
     elif language == Language.EN.value:
-        nature_words = initialize_en()
-    return nature_words
+        initialize_en()
+
+    return id_word_info_dict
 
 # can we get a list here? or hug magic
 # language should be in ?language=de and should be read automatically here
+# how is the resouce url (word list) passed?
 @app.post("/metrics/")
-def process_text(text: list[str], language:str) -> dict[str, dict[str, dict[str,int]]]:
-    nature_words = setup_analysis_components(language)
+def process_text(text: list[str], language:str, resource_url:str) -> dict[str, dict[str, dict[str,int]]]:
+    word_info = setup_analysis_components(language, resource_url)
+    unique_words = set(word_info.keys())
+
     # annotate
-    segment_to_frq = {}
+    word_to_chapter_frq = {}
     for i, segment in enumerate(nlp.pipe(text, disable=["parser", "ner"])):
         lemmatized_text = [token.lemma_ for token in segment]
+
+        # count and intersect
         vocabulary = set(lemmatized_text)
         counted = Counter(lemmatized_text)
+        intersect = unique_words.intersection(vocabulary)
 
-        result = {}
-        # for word in nature_words[nature_category]:
-        for description, word_list in nature_words.items():
-            category_result = {}
-            intersect = word_list.intersection(vocabulary)
-            for word in intersect:
-                category_result[word] = counted[word]
-            result[description] = category_result
-        segment_to_frq[i] = result
-    return segment_to_frq
-
-# TODO add option for selecting which nature category to get the frequencies for and only pass that to the process segment function 
-
+        # save frequencies
+        for word in intersect:
+            if word not in word_to_chapter_frq:
+                word_to_chapter_frq[word] = word_info[word]
+                word_to_chapter_frq[word][SEGMENT_FRQ] = {}
+            word_to_chapter_frq[word][SEGMENT_FRQ][i] = counted[word]
+    
+    for word, info in word_to_chapter_frq.items():
+        word_to_chapter_frq[word][OVERALL_FRQ] = sum(info[SEGMENT_FRQ].values())
+    print(word_to_chapter_frq)
+    return word_to_chapter_frq 
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) != 2:
-        print(f"usage: {args[0]} path/to/test/file")
+    if len(args) != 4:
+        print(f"usage: {args[0]} path/to/test/file en||de url/to/wordlist")
         exit(-1)
     with open(args[1]) as txt_in:
         segments = txt_in.readlines()
 
-    process_text(segments, "de")
+    process_text(segments, args[2], args[3])
